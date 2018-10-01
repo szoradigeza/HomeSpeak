ca4af3a81ae6e3508d3a63bd200948bcae91cf09
Réthalmi Dávid <david.rethalmi@gmail.com>
2018-10-01 19:01:11 +0200

    Changes to the TCP server

--- server/tcpserver.cpp
+++ server/tcpserver.cpp
@@ -1,33 +1,95 @@
-#include <QtWidgets/QMessageBox>
+#include <QtNetwork>
+#include <QtCore>
+#include <QDebug>
 #include "tcpserver.h"
 
 TcpServer::TcpServer(QWidget *parent) {
-    tcpServer = new QTcpServer(this);
-    if (!tcpServer->listen()) {
-        QMessageBox::critical(this, tr("Fortune Server"),
-                              tr("Unable to start the server: %1.")
-                                      .arg(tcpServer->errorString()));
-        close();
-        return;
+    QNetworkConfigurationManager manager;
+    if (manager.capabilities() & QNetworkConfigurationManager::NetworkSessionRequired) {
+        // Get saved network configuration
+        QSettings settings(QSettings::UserScope, QLatin1String("QtProject"));
+        settings.beginGroup(QLatin1String("QtNetwork"));
+        const QString id = settings.value(QLatin1String("DefaultNetworkConfiguration")).toString();
+        settings.endGroup();
+
+        // If the saved network configuration is not currently discovered use the system default
+        QNetworkConfiguration config = manager.configurationFromIdentifier(id);
+        if ((config.state() & QNetworkConfiguration::Discovered) !=
+            QNetworkConfiguration::Discovered) {
+            config = manager.defaultConfiguration();
+        }
+
+        networkSession = new QNetworkSession(config, this);
+        connect(networkSession, &QNetworkSession::opened, this, &TcpServer::sessionOpened);
+
+        statusLabel->setText(tr("Opening network session."));
+        networkSession->open();
+    } else {
+        sessionOpened();
     }
 
+    fortunes << tr("You've been leading a dog's life. Stay off the furniture.")
+             << tr("You've got to think about tomorrow.")
+             << tr("You will be surprised by a loud noise.")
+             << tr("You will feel hungry again in another hour.")
+             << tr("You might have mail.")
+             << tr("You cannot kill time without injuring eternity.")
+             << tr("Computers are not intelligent. They only think they are.");
+
+}
+
+void TcpServer::sessionOpened() {
+    // Save the used configuration
+    if (networkSession) {
+        QNetworkConfiguration config = networkSession->configuration();
+        QString id;
+        if (config.type() == QNetworkConfiguration::UserChoice)
+            id = networkSession->sessionProperty(QLatin1String("UserChoiceConfiguration")).toString();
+        else
+            id = config.identifier();
+
+        QSettings settings(QSettings::UserScope, QLatin1String("QtProject"));
+        settings.beginGroup(QLatin1String("QtNetwork"));
+        settings.setValue(QLatin1String("DefaultNetworkConfiguration"), id);
+        settings.endGroup();
+    }
+
+    tcpServer = new QTcpServer();
+    if (!tcpServer->listen()) {
+        qDebug() << QStringLiteral("Unable to start the server\n"
+                                   "Reason:\n"
+                                   " {0}").arg(tcpServer->errorString());
+        return;
+    }
     QString ipAddress;
     QList<QHostAddress> ipAddressesList = QNetworkInterface::allAddresses();
-
     // use the first non-localhost IPv4 address
-    for (int i = 0; i < ipAddressesList.size(); ++i) {
-        if (ipAddressesList.at(i) != QHostAddress::LocalHost &&
-            ipAddressesList.at(i).toIPv4Address()) {
-            ipAddress = ipAddressesList.at(i).toString();
+    for (const auto &i : ipAddressesList) {
+        if (i != QHostAddress::LocalHost &&
+                i.toIPv4Address()) {
+            ipAddress = i.toString();
             break;
         }
     }
-
     // if we did not find one, use IPv4 localhost
     if (ipAddress.isEmpty())
         ipAddress = QHostAddress(QHostAddress::LocalHost).toString();
     statusLabel->setText(tr("The server is running on\n\nIP: %1\nport: %2\n\n"
                             "Run the Fortune Client example now.")
                                  .arg(ipAddress).arg(tcpServer->serverPort()));
+}
 
+void TcpServer::sendFortune() {
+    QByteArray block;
+    QDataStream out(&block, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_5_10);
+
+    out << fortunes[QRandomGenerator::global()->bounded(fortunes.size())];
+
+    QTcpSocket *clientConnection = tcpServer->nextPendingConnection();
+    connect(clientConnection, &QAbstractSocket::disconnected,
+            clientConnection, &QObject::deleteLater);
+
+    clientConnection->write(block);
+    clientConnection->disconnectFromHost();
 }
--- server/tcpserver.h
+++ server/tcpserver.h
@@ -4,14 +4,20 @@
 #include <QtWidgets/QWidget>
 #include <QtWidgets/QLabel>
 
+class QLabel;
+class QTcpServer;
+class QNetworkSession;
+
 class TcpServer : public QDialog {
     Q_OBJECT
+
 public:
     TcpServer(QWidget * parent = nullptr);
 
 public slots:
     void sessionOpened();
     void sendFortune();
+
 private:
     QLabel *statusLabel = nullptr;
     QTcpServer *tcpServer = nullptr;
